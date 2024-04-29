74ecce4b0ddfc21178392e79e315dfb0d9a540cb
Keago99 <keatabde@gmail.com>
2024-04-29 19:29:52 +0200

    Added color scheme and input.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,22 @@
-
-
 const apiKey = "096c2b0f2703437eaad134447242904";
 const apiURL = "http://api.weatherapi.com/v1/current.json?key=096c2b0f2703437eaad134447242904&q=Cape Town";
+const insertWeather = document.getElementById("insertWeather");
+const insertTempC = document.getElementById("insertTempC");
 
 
 fetch(apiURL, {mode: 'cors'})
-.then(function(response) {
-    console.log(response.json());
+.then(function(response){
+    return response.json();
+})
+.then(function(response){
+    console.log(response);
+    const weatherType = (response.current.condition.text);
+    const weatherTempC = (response.current.feelslike_c);
+    insertWeather.innerHTML+= weatherType;
+    insertTempC.innerHTML+= weatherTempC;
 });
+
+
+
+
+
