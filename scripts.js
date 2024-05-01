2fd08654052e26250c3feae2abca17999190a77d
Keago99 <keatabde@gmail.com>
2024-05-01 16:05:49 +0200

    Successfully added eventlistener to clickable button, retread but its progress.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,30 @@
 const apiKey = "096c2b0f2703437eaad134447242904";
-const apiURL = "http://api.weatherapi.com/v1/current.json?key=096c2b0f2703437eaad134447242904&q=Cape Town";
+const apiURL = "http://api.weatherapi.com/v1/current.json?key=096c2b0f2703437eaad134447242904&q=Dublin";
 const insertWeather = document.getElementById("insertWeather");
 const insertTempC = document.getElementById("insertTempC");
+const weatherButton = document.getElementById("weatherButton");
 
+function callWeather(){
+        fetch(apiURL, {mode: 'cors'})
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+        console.log(response);
+        const weatherType = (response.current.condition.text);
+        const weatherTempC = (response.current.feelslike_c);
+        insertWeather.innerHTML+= weatherType;
+        insertTempC.innerHTML+= weatherTempC;
+    });
+}
 
-fetch(apiURL, {mode: 'cors'})
-.then(function(response){
-    return response.json();
+weatherButton.addEventListener("click", () => {
+    callWeather();
 })
-.then(function(response){
-    console.log(response);
-    const weatherType = (response.current.condition.text);
-    const weatherTempC = (response.current.feelslike_c);
-    insertWeather.innerHTML+= weatherType;
-    insertTempC.innerHTML+= weatherTempC;
-});
+
+
+
+
 
 
 
