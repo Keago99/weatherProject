33135482c3a46594c47251f5913f74ab0ee8226d
Keago99 <keatabde@gmail.com>
2024-05-02 16:02:40 +0200

    added location div and updated from json to show location.

--- scripts.js
+++ scripts.js
@@ -1,32 +1,53 @@
 const apiKey = "096c2b0f2703437eaad134447242904";
-const apiURL = "http://api.weatherapi.com/v1/current.json?key=096c2b0f2703437eaad134447242904&q=Dublin";
+const apiURL = "http://api.weatherapi.com/v1/current.json?key=096c2b0f2703437eaad134447242904&q=";
 const insertWeather = document.getElementById("insertWeather");
 const insertTempC = document.getElementById("insertTempC");
 const weatherButton = document.getElementById("weatherButton");
+const insertLocation = document.getElementById("insertLocation");
+var newAPIURL = "";
 
 function callWeather(){
-    try{
-        fetch(apiURL, {mode: 'cors'})
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(response){
-            console.log(response);
-            const weatherType = (response.current.condition.text);
-            const weatherTempC = (response.current.feelslike_c);
-            insertWeather.innerHTML= weatherType;
-            insertTempC.innerHTML= weatherTempC;
-        })
-    }
-    catch{
-        console.error("didn't work");
-    }
 
+    // Simplfying a .then() route for this apicall
+    fetch(newAPIURL, {mode: 'cors'})
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+        console.log(response);
+        const weatherType = (response.current.condition.text);
+        const weatherTempC = (response.current.feelslike_c);
+        const locationName = (response.location.name);
+        insertWeather.innerHTML= weatherType;
+        insertTempC.innerHTML= weatherTempC;
+        insertLocation.innerHTML = locationName;
+
+    })
+    .catch(function(err){
+        alert("the error iz: " + err);
+    });
+}
+
+function amendCall(){
+    const locationText = document.getElementById("locationText").value;
+    if (locationText == null)
+    {
+        alert("Null");
+    }
+    else if (locationText == ""){
+        alert("please enter a location");
+    }
+    else{
+        newAPIURL = (apiURL + locationText);
+    }
 }
 
 weatherButton.addEventListener("click", () => {
+    amendCall();
     callWeather();
 })
+
+
 
 
 
